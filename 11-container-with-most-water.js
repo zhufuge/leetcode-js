c43e8017e84f668fbb9ad5e59678dd58c1d8d4e3
6ml <344655454@qq.com>
2017-11-22 15:10:48 +0800

    11th need to more explain & Add 654

--- 11-container-with-most-water.js
+++ 11-container-with-most-water.js
@@ -1,5 +1,5 @@
 // 11. Container With Most Water
-// Medium 36% locked:false
+// Medium   36%
 
 // Given n non-negative integers a1, a2, ..., an, where each represents a point
 // at coordinate (i, ai). n vertical lines are drawn such that the two endpoints
@@ -12,30 +12,30 @@
  * @param {number[]} height
  * @return {number}
  */
-const maxArea0 = function(height) {
-  const n = height.length
-  if (n <= 1) return 0
-
-  let result = 0, max = 0
-  for (let i = 0; i < n; i++) {
-
-    for (let j = i + 1; j < n; j++) {
-      const area = Math.min(height[i], height[j]) * (j - i)
-      if (area > result) result = area
-    }
-  }
-
-  return result
-}
-
 const maxArea = function(height) {
   let result = 0, i = 0, j = height.length - 1
   while (i < j) {
-    const min = height[i] > height[j] ? true : false
-    result = Math.max(result, height[min ? j : i] * (j - i))
-    if (min) j--
+    const min = Math.min(height[i] , height[j])
+    result = Math.max(result, min * (j - i))
+    if (min === height[j]) j--
     else i++
   }
-
   return result
 }
+
+;[
+  [1, 2, 1, 3, 4, 2, 1],        // 8
+].forEach(height => {
+  console.log(maxArea(height))
+})
+
+// Solution:
+// 从两侧开始向中间遍历，
+// 两侧中小的哪一个向中间前进（相等的话可随意一边）。
+// 在遍历过程中每次计算一次构成的容器的容量，保存最大的作为答案。
+
+// 为什么小的一侧前进？这会不会错过最大的容器呢？
+// 有待严谨的分析 ;p
+// TODO
+
+// Submission Result: Accepted
---
+++ 654-maximum-binary-tree.js
@@ -0,0 +1,72 @@
+// 654. Maximum Binary Tree
+// Medium   70%
+
+
+// Given an integer array with no duplicates. A maximum tree building on this
+// array is defined as follow:
+
+// The root is the maximum number in the array.
+// The left subtree is the maximum tree constructed from left part subarray
+// divided by the maximum number.
+// The right subtree is the maximum tree constructed from right part subarray
+// divided by the maximum number.
+
+// Construct the maximum tree by the given array and output the root node of this
+// tree.
+
+// Example 1:
+
+// Input: [3,2,1,6,0,5]
+// Output: return the tree root node representing the following tree:
+
+//       6
+//     /   \
+//    3     5
+//     \    /
+//      2  0
+//        \
+//         1
+
+// Note:
+
+// The size of the given array will be in the range [1,1000].
+
+/**
+ * Definition for a binary tree node.
+ */
+function TreeNode(val) {
+  this.val = val
+  this.left = this.right = null
+}
+
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+const constructMaximumBinaryTree = function(nums) {
+  function iter(start, end) {
+    if (start > end) return null
+    let j = start
+    for (let i = start; i <= end; i++) {
+      j = nums[i] > nums[j] ? i : j
+    }
+    const root = new TreeNode(nums[j])
+    root.left = iter(start, j - 1)
+    root.right = iter(j + 1, end)
+    return root
+  }
+  return iter(0, nums.length - 1)
+}
+
+;[
+  [3,2,1,6,0,5],
+].forEach(nums => {
+  console.log(constructMaximumBinaryTree(nums))
+})
+
+// Solution:
+// 递归过程中，
+// 每次找出数组的最大值位置，并为最大值创建一个节点作为根节点返回，
+// 且该节点的左（右）子树为该数字位置的左（右）边子数组。
+
+// Submission Result: Accepted
