5d865543ce6c5f249c4217e250ede28f9544ca07
6ml <344655454@qq.com>
2017-12-20 14:46:04 +0800

    Maybe you need a purpose.

--- 68-text-justification.js
+++ 68-text-justification.js
@@ -1,8 +1,8 @@
 // 68. Text Justification
-// Hard 19% locked:false
+// Hard   19%
 
-//  Given an array of words and a length L, format the text such that each line
-//  has exactly L characters and is fully (left and right) justified.
+// Given an array of words and a length L, format the text such that each line
+// has exactly L characters and is fully (left and right) justified.
 
 // You should pack your words in a greedy approach; that is, pack as many words
 // as you can in each line. Pad extra spaces ' ' when necessary so that each
@@ -32,8 +32,8 @@
 // click to show corner cases.
 // Corner Cases:
 
-//     A line other than the last line might contain only one word. What should
-//     you do in this case? In this case, that line should be left-justified.
+// A line other than the last line might contain only one word. What should
+// you do in this case? In this case, that line should be left-justified.
 
 /**
  * @param {string[]} words
@@ -41,43 +41,47 @@
  * @return {string[]}
  */
 const fullJustify = function(words, maxWidth) {
-  if (words === null || words === void 0) return words
-  if (maxWidth === 0) return words
-
-  const res = []
-  let group = [], width = 0
-  for (let word of words) {
-    if (width + word.length + group.length> maxWidth) {
-      console.log(group);
-      let n = group.length, spaces = maxWidth - width
-      if (n === 1) res.push(group[0] + ' '.repeat(spaces))
-      else {
-        let s = ''
-        for (let w of group) {
-          const space = Math.trunc((spaces - 1) / (--n)) + 1
-          s += w + (spaces > 0 ? ' '.repeat(Math.min(spaces, space)) : '')
-          spaces -= space
-        }
-        res.push(s)
-      }
-
-      group = [word]
-      width = word.length
-    } else {
-      group.push(word)
-      width += word.length
+  const result = [], n = words.length
+  for (let i = 0, j = 0; i < n; i += j, j = 0) {
+    let width = 0
+    while (i + j < n && width + words[i + j].length + j <= maxWidth) {
+      width += words[i + j].length
+      j++
     }
+    let string = '', spaces = maxWidth - width
+    for (let k = 0; k < j - 1; k++) {
+      string += words[i + k]
+      if (i + j >= n) {
+        string += ' '
+        spaces -= 1
+      } else {
+        const repeat = spaces > 0 ? Math.ceil(spaces / (j - k - 1)) : 0
+        spaces -= repeat
+        string += ' '.repeat(repeat)
+      }
+    }
+    string += words[i + j - 1] + ' '.repeat(spaces)
+    result.push(string)
   }
 
-  let s = '', spaces = maxWidth - group.length - width
-  for (let w of group) s += w + (spaces >= 0 ? ' ' : '')
-  s += ' '.repeat(spaces > 0 ? spaces : 0)
-  res.push(s)
-
-  return res
+  return result
 }
 
-//console.log(fullJustify(["This", "is", "an", "example", "of", "text", "justification."], 16))
-console.log(fullJustify(['a', 'b', 'c', 'd', 'e', 'f', 'g'], 1))
-// console.log(fullJustify(["What","must","be","shall","be."], 12))
-// console.log(fullJustify(["a"], 2))
+;[
+  [["This", "is", "an", "example", "of", "text", "justification."], 16],
+  [['a', 'b', 'c', 'd', 'e', 'f', 'g'], 1],
+  [["What","must","be","shall","be."], 12],
+  [["a"], 2],
+].forEach(([words, maxWidth]) => {
+  console.log(fullJustify(words, maxWidth))
+})
+
+// Solution:
+// 每个单词间必须有空格隔开，最小为1，
+// 每次加一个字符串的长度，这些加起来的长度加上最小空格数小于或等于 maxWidth。
+// 当数量最大时，将这些单词构造出句子。
+// 此时，单词将的间隔的计算为
+// 剩下空格数除以剩下单词数，并向上取整。
+// 若为最后一行，则单词间只加一个空格，并将剩下的所有空格加到末尾。
+
+// Submission Result: Accepted
