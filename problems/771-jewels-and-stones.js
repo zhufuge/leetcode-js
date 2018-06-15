0c03d05c1e060591fdcf6974be25e91838218727
zhufuge <zhufuge@outlook.com>
2018-06-15 16:26:20 +0800

    add launch.json

---
+++ problems/771-jewels-and-stones.js
@@ -0,0 +1,52 @@
+// 771. Jewels and Stones
+// easy 82%
+
+// You're given strings J representing the types of stones that are jewels, and
+// S representing the stones you have.  Each character in S is a type of stone
+// you have.  You want to know how many of the stones you have are also jewels.
+
+// The letters in J are guaranteed distinct, and all characters in J and S are
+// letters. Letters are case sensitive, so "a" is considered a different type
+// of stone from "A".
+
+// Example 1:
+
+// Input: J = "aA", S = "aAAbbbb"
+// Output: 3
+
+// Example 2:
+
+// Input: J = "z", S = "ZZ"
+// Output: 0
+
+// Note:
+//   - S and J will consist of letters and have length at most 50.
+//   - The characters in J are distinct.
+
+/**
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
+ */
+const numJewelsInStones = function(J, S) {
+  const hash = {}
+  for (let j of J) hash[j] = true
+  let result = 0
+  for (let s of S) {
+    if (hash[s]) result++
+  }
+  return result
+}
+
+;[
+  ['aA', 'aAAbbbb'],
+  ['z', 'ZZ'],
+].forEach(args => {
+  console.log(numJewelsInStones(...args))
+})
+
+// Solution:
+// 使用一个哈希表保存宝石种类，
+// 再判断 S 中是否有该宝石。
+
+// Submission Result: Accepted
