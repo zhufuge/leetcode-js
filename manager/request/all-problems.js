510ff0ebb900e2a5cb8fe98fb1a27a3b55a907af
zhufuge <zhufuge@outlook.com>
2020-01-15 16:30:41 +0800

    remove problems json

--- manager/request/all-problems.js
+++ manager/request/all-problems.js
@@ -15,7 +15,7 @@ function main(callback) {
     console.log('Request OK!')
     const json = JSON.parse(body)
     const problems = json['stat_status_pairs']
-    fs.writeFileSync(PROBLEMS_STORE_PATH, JSON.stringify(problems), 'utf8')
+    fs.writeFileSync(PROBLEMS_STORE_PATH, JSON.stringify(problems, null, 2), 'utf8')
     console.log('Request problems total:', problems.length)
 
     if (callback) callback(error, response, body)
